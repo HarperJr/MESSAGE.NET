1ecbd2ec15bc0e7f12d242fda67dffdcc44da966
HarperJr <doctorHarperjr@gmail.com>
2018-11-10 14:53:51 +0300

    #2 UI/UX Development
    - Added sign up page

---
+++ Messanger/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Messanger.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
